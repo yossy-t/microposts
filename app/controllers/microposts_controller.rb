45526c65790343ffac3a96317ab2bea47afecdf6
hiroto <yossy.goodcolor@gmail.com>
2018-02-08 07:54:32 +0000

    post

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,24 @@
+class MicropostsController < ApplicationController
+  before_action :require_user_logged_in
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+end
